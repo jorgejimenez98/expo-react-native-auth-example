cdf6057a2e465d648e3094dae226cf0db9969f00
Jorge Jimenez <georgeguitarra07@gmail.com>
2024-10-07 04:26:39 +0200

    feat(hooks): Added useAsyncStorage hook

---
+++ src/hooks/useAsyncStorage.ts
@@ -0,0 +1,54 @@
+import { useState, useEffect, useCallback } from "react"
+
+import AsyncStorage from "@react-native-async-storage/async-storage"
+
+export function useAsyncStorage<T>(key: string, initialValue: T) {
+  const [storedValue, setStoredValue] = useState<T | null>(null)
+
+  // Get value from AsyncStorage
+  const getValue = useCallback(async () => {
+    try {
+      const value = await AsyncStorage.getItem(key)
+      if (value !== null) {
+        setStoredValue(JSON.parse(value))
+      } else if (initialValue !== undefined) {
+        setStoredValue(initialValue)
+      }
+    } catch (error) {
+      console.error(`Error Get AsyncStorage: ${key}`, error)
+    }
+  }, [key, initialValue])
+
+  // Save value on AsyncStorage
+  const setValue = async (value: T) => {
+    try {
+      await AsyncStorage.setItem(key, JSON.stringify(value))
+      setStoredValue(value)
+    } catch (error) {
+      console.error(`Error Set AsyncStorage: ${key}`, error)
+    }
+  }
+
+  // Delete value from async storage
+  const removeValue = async () => {
+    try {
+      await AsyncStorage.removeItem(key)
+      setStoredValue(null)
+    } catch (error) {
+      console.error(`Error Delete AsyncStorage: ${key}`, error)
+    }
+  }
+
+  useEffect(() => {
+    getValue()
+  }, [getValue])
+
+  return {
+    // State
+    value: storedValue,
+
+    // Functions
+    setValue,
+    removeValue
+  }
+}
---
+++ src/lib/constants/ui.constants.ts
@@ -0,0 +1,7 @@
+export const UI = {
+  LOCALSTORAGE: {
+    USER_TOKEN_KEY: "user-token"
+  }
+}
+
+Object.freeze(UI)
